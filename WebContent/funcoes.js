8454eafd301ed22f43c3e1c004153b70f2cb1227
virmerson <virmerson@gmail.com>
2016-04-14 22:18:21 -0400

    Curso Java Script Basico UCDB

---
+++ WebContent/funcoes.js
@@ -0,0 +1,15 @@
+var imagem = document.getElementById("foto");
+			
+			function aumentar(){
+					
+					imagem.height=200;
+					imagem.width=200;
+			}
+			
+			
+			function diminuir(){
+					
+					imagem.height=100;
+					imagem.width=100;
+			}
+			
